a4eedeae618904b44bdf95f96be8ad7f41480e64
Samanja Cartagena <c.samanja09@Pabs.local>
2023-10-14 03:33:12 -0400

    skills showing

--- webscraping/main.py
+++ webscraping/main.py
@@ -11,4 +11,11 @@ import requests
 
 html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
 soup = BeautifulSoup(html_text,'lxml')
-job=soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
+job=soup.find('li',class_='clearfix job-bx wht-shd-bx')
+company_name=job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+skills=job.find('span',class_='srp-skills').text.replace(' ','')
+
+print(f'''
+Company name :{company_name}  
+Required Skills:{skills}
+''')
